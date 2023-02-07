df4744da48d8cbd52de5b293e180717ade62d871
Marissa Lewinski <101777634+Mari-Lew@users.noreply.github.com>
2023-02-07 12:06:53 -0600

    121. SetBrushStroke function added

--- app/src/main/java/com/example/canvasapp/DrawingView.kt
+++ app/src/main/java/com/example/canvasapp/DrawingView.kt
@@ -3,6 +3,7 @@ package com.example.canvasapp
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 
@@ -101,6 +102,18 @@ class DrawingView(context: Context, attribs: AttributeSet): View(context, attrib
         invalidate() //invalidates the whole view
         return true
 
+    }
+
+    fun setBrushSize(newSize: Float)
+    {
+        //want to take screen size into consideration
+        mBrushSize = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            newSize,
+            resources.displayMetrics
+            )
+
+        mDrawPaint!!.strokeWidth = mBrushSize
     }
 
     /**
--- app/src/main/java/com/example/canvasapp/MainActivity.kt
+++ app/src/main/java/com/example/canvasapp/MainActivity.kt
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private var drawView: DrawingView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawView = findViewById(R.id.drawingView)
+        //drawView?.setBrushSize(20.toFloat())
     }
 }
