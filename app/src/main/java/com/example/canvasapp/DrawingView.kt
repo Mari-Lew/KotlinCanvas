d84e4b3d8ae0ab9442edca687d5ceccc13cd0197
Marissa Lewinski <101777634+Mari-Lew@users.noreply.github.com>
2023-02-07 11:24:53 -0600

    118. Drawing View finished - ability to draw on screen

--- app/src/main/java/com/example/canvasapp/DrawingView.kt
+++ app/src/main/java/com/example/canvasapp/DrawingView.kt
@@ -3,6 +3,7 @@ package com.example.canvasapp
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 
 /**
@@ -38,6 +39,56 @@ class DrawingView(context: Context, attribs: AttributeSet): View(context, attrib
 
         //Set brush size
         mBrushSize = 10.toFloat()
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        mCanvasBitmap = Bitmap.createBitmap(w,h, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(mCanvasBitmap!!)
+    }
+
+    override fun onDraw(drawCanvas: Canvas) {
+        super.onDraw(drawCanvas)
+        drawCanvas.drawBitmap(mCanvasBitmap!!,0f,0f,mCanvasPaint)
+
+        if(!mDrawPath!!.isEmpty)
+        {
+            mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
+            mDrawPaint!!.color = mDrawPath!!.color
+            drawCanvas.drawPath(mDrawPath!!, mDrawPaint!!)
+        }
+
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val touchX = event?.x
+        val touchY = event?.y
+
+        when(event?.action) //same 3 things when using a motion event
+        {
+            //Finger on screen
+                MotionEvent.ACTION_DOWN -> {
+                    mDrawPath!!.color = color
+                    mDrawPath!!.brushThickness = mBrushSize
+
+                    mDrawPath!!.reset() //clear any lines
+                    mDrawPath!!.moveTo(touchX!!,touchY!!)
+                }
+            //Dragging it to draw
+                MotionEvent.ACTION_MOVE ->
+                {
+                    mDrawPath!!.lineTo(touchX!!,touchY!!)
+                }
+            //Release the touch
+                MotionEvent.ACTION_UP ->
+                {
+                    mDrawPath = CustomPath(color, mBrushSize)
+                }
+            else -> return false
+        }
+        invalidate() //invalidates the whole view
+        return true
+
     }
 
     /**
